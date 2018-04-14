b2c310814cf06020c0367c8905380d3d545f3eee
johngalay <jadgalay@gmail.com>
2018-04-14 22:36:42 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 6;
+
+	private Rigidbody2D myRigidbody;
+	private Camera viewCamera;
+	private Vector2 velocity;
+
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+		viewCamera = Camera.main;
+	}
+	
+	void Update () {
+		Vector3 pointToLook = viewCamera.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 direction = new Vector2(
+			pointToLook.x - transform.position.x,
+			pointToLook.y - transform.position.y
+		);
+		transform.up = direction;
+		velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+	}
+
+	void FixedUpdate() {
+		myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+	}
+}
